7cd54fde62ae51452cdb423d5ca82d80496826c5
bagasartmr <81bagasartamara@gmail.com>
2025-06-17 18:41:25 +0700

    setting wrangler

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/api', (c) => {
+  return c.text('hello');
+})
+
+// fallback ke frontend
+app.get('*', (c) => c.env.ASSETS.fetch(c.req.raw));
+
+export default app;
