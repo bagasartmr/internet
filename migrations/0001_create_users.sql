92cf0e6b3333162d10341ddcf175043d74a45f2d
bagasartmr <81bagasartamara@gmail.com>
2025-06-17 19:34:28 +0700

    install dan setting file users.sql

---
+++ migrations/0001_create_users.sql
@@ -0,0 +1,9 @@
+-- Migration number: 0001 	 2025-06-17T12:30:37.559Z
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    address TEXT
+);
+
+INSERT INTO users(name, address) VALUES ('Andre', 'Purwosari'), ('Andian', 'Sukorejo'), ('Ana', 'Pandaaan');
