102fc01e3c49786f375eb72c1077c1ce2cddcc22
bagasartmr <81bagasartamara@gmail.com>
2025-06-17 19:25:39 +0700

    setting api users

--- api/index.js
+++ api/index.js
@@ -6,6 +6,12 @@ app.get('/api', (c) => {
   return c.text('hello');
 })
 
+app.get('/api/users', async (c) => {
+  let { results } = await c.env.DB.prepare("SELECT * FROM users").all()
+  return c.json(results)
+})
+
+
 // fallback ke frontend
 app.get('*', (c) => c.env.ASSETS.fetch(c.req.raw));
 
