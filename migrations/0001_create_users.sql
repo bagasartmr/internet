9a16a64e7f915a37b2790e8939f461ce863d07f6
bagasartmr <81bagasartamara@gmail.com>
2025-06-23 16:35:55 +0700

    setting database sql

--- migrations/0001_create_users.sql
+++ migrations/0001_create_users.sql
@@ -3,7 +3,9 @@
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    address TEXT
+    number TEXT,
+    address TEXT,
+    package TEXT
 );
 
-INSERT INTO users(name, address) VALUES ('Andre', 'Purwosari'), ('Andian', 'Sukorejo'), ('Ana', 'Pandaaan');
+INSERT INTO users(name, number, address, internet) VALUES ('Andre', '085736', 'Purwosari', '15 Mbps-Rp. 165.000'), ('Andian', '202369', 'Sukorejo', '25 Mbps-Rp. 250.000'), ('Ana', '690087', 'Pandaaan', '50 Mbps-Rp. 450.000');
