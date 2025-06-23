e72dcc60eb7b015e29b6229eb572b644b9ecd1a4
bagasartmr <81bagasartamara@gmail.com>
2025-06-23 16:19:26 +0700

    stting api

--- api/index.js
+++ api/index.js
@@ -6,16 +6,11 @@ app.get('/api', (c) => {
   return c.text('hello');
 })
 
-app.get('/api/users', (c) => {
-  return c.text('users bagas coba lagi');
-})
-
-
-/*app.get('/api/users', async (c) => {
+app.get('/api/users', async (c) => {
   let { results } = await c.env.DB.prepare("SELECT * FROM users").all()
   return c.json(results)
 })
-*/
+
 
 // fallback ke frontend
 app.get('*', (c) => c.env.ASSETS.fetch(c.req.raw));
