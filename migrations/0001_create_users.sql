fe213757cc6ec9384d22685ddf14db2680bb24c7
bagasartmr <81bagasartamara@gmail.com>
2025-06-23 16:36:34 +0700

    update database

--- migrations/0001_create_users.sql
+++ migrations/0001_create_users.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     name TEXT NOT NULL,
     number TEXT,
     address TEXT,
-    package TEXT
+    internet TEXT
 );
 
 INSERT INTO users(name, number, address, internet) VALUES ('Andre', '085736', 'Purwosari', '15 Mbps-Rp. 165.000'), ('Andian', '202369', 'Sukorejo', '25 Mbps-Rp. 250.000'), ('Ana', '690087', 'Pandaaan', '50 Mbps-Rp. 450.000');
