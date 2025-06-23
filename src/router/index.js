d5e41ad785544b04f16ba362e193fbe83a7909b6
bagasartmr <81bagasartamara@gmail.com>
2025-06-23 21:10:45 +0700

    ubah router

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,14 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/products',
+      name: 'create-product',
+      component: () => import('../views/CreateProductView.vue'),
+    },
+    {
+      path: '/products/:id',
+      name: 'edit-product',
+      component: () => import('../views/EditProductView.vue'),
     },
   ],
 })
