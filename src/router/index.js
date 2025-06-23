3f00066248e8514f818eb8478e574bfeb3a745de
bagasartmr <81bagasartamara@gmail.com>
2025-06-23 22:45:00 +0700

    coba ulang

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: '/products',
       name: 'create-product',
-      component: () => import('../views/CreateProductView.vue'),
+      component: () => import('../views/CreateProduct.vue'),
     },
     {
       path: '/products/:id',
