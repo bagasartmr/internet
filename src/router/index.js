61f93615f0d2109f7184defbd86e8c4887c58e10
bagasartmr <81bagasartamara@gmail.com>
2025-06-25 19:53:45 +0700

    delete wifilist

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WifiForm from '../views/WifiForm.vue'
-import WifiList from '../views/WifiList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
