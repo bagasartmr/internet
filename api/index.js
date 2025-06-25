ea878f2c0e558f378063f91190eedcfec4b6cbaa
bagasartmr <81bagasartamara@gmail.com>
2025-06-25 19:27:32 +0700

    setting agar data ditampilkan

--- api/index.js
+++ api/index.js
@@ -2,6 +2,11 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
+app.get('/api/wifi', async (c) => {
+  const result = await c.env.DB.prepare('SELECT * FROM wifi').all();
+  return c.json(result.results);
+});
+
 app.post('/api/wifi', async (c) => {
   const input = await c.req.json();
   const query = `
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WifiForm from '../views/WifiForm.vue'
+import WifiList from '../views/WifiList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-  {
+      {
       path: '/',
       name: 'home',
       component: WifiForm,
     },
+    {
+      path: '/',
+      name: 'wifi-list',
+      component: WifiList
+    }
     ]
 })
 
