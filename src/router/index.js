5f412893736d889a5e6b971009fae8c46e975df7
bagasartmr <81bagasartamara@gmail.com>
2025-06-25 18:38:07 +0700

    setting route

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import WifiForm from '../views/WifiForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/products',
-      name: 'create-product',
-      component: () => import('../views/CreateProduct.vue'),
-    },
-    {
-      path: '/products/:id',
-      name: 'edit-product',
-      component: () => import('../views/EditProductView.vue'),
-    },
+  {
+  path: '/create',
+  name: 'wifi-create',
+  component: WifiForm
+}
   ],
 })
 
