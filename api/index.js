e8eb1f46d5b6b34721920f37d1678e319a93a399
bagasartmr <81bagasartamara@gmail.com>
2025-06-25 18:44:14 +0700

    setting api

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
-app.post('/api/products', async (c) => {
-  const input = await c.req.json()
-  const query = `INSERT INTO products(name, price) VALUES (?, ?)`
-  const result = await c.env.DB.prepare(query).bind(input.name, input.price).run()
-  return c.json({ success: true, id: result.lastInsertRowId })
-})
+app.post('/api/wifi', async (c) => {
+  const input = await c.req.json();
+  const query = `
+    INSERT INTO wifi (name, nohp, alamat, paket)
+    VALUES (?, ?, ?, ?)`;
+    
+  const result = await c.env.DB.prepare(query)
+    .bind(input.name, input.nohp, input.alamat, input.paket)
+    .run();
+
+  return c.json({ success: true, result });
+});
 
 export default app;
