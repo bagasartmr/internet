8c565c6b6dfccef09cb3ff58b00bc79fac6d544a
bagasartmr <81bagasartamara@gmail.com>
2025-06-27 21:50:11 +0700

    setting api edit

--- api/index.js
+++ api/index.js
@@ -24,6 +24,22 @@ app.post('/api/wifi', async (c) => {
   return c.json({ success: true, result });
 })
 
+app.put('/api/wifi/:id', async (c) => {
+  const id = c.req.param('id')
+  const input = await c.req.json()
+
+  const query = `
+    UPDATE wifi
+    SET name = ?, nohp = ?, alamat = ?, paket = ?
+    WHERE id = ?`
+
+  const result = await c.env.DB.prepare(query)
+    .bind(input.name, input.nohp, input.alamat, input.paket, id)
+    .run()
+
+  return c.json({ success: true, result })
+})
+
 app.delete('/api/wifi/:id', async (c) => {
   const id = c.req.param('id');
 
